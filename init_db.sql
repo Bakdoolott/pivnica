b9cda681115df742468e7bff1a3c28dbe55e2fe6
Bakdoolott <mirlanuulubakdoolot@gmail.com>
2026-07-31 19:26:38 +0600

    fixed db creating

--- init_db.sql
+++ init_db.sql
@@ -1,14 +1,18 @@
 CREATE DATABASE auth_db;
 CREATE DATABASE core_db;
 
-
 CREATE USER auth_user WITH PASSWORD 'auth_strong_pass_123';
 CREATE USER core_user WITH PASSWORD 'core_strong_pass_456';
 
-
-GRANT CONNECT ON DATABASE auth_db TO auth_user;
 GRANT ALL PRIVILEGES ON DATABASE auth_db TO auth_user;
-
-
-GRANT CONNECT ON DATABASE core_db TO core_user;
 GRANT ALL PRIVILEGES ON DATABASE core_db TO core_user;
+
+\connect auth_db
+
+GRANT USAGE, CREATE ON SCHEMA public TO auth_user;
+ALTER SCHEMA public OWNER TO auth_user;
+
+\connect core_db
+
+GRANT USAGE, CREATE ON SCHEMA public TO core_user;
+ALTER SCHEMA public OWNER TO core_user;
