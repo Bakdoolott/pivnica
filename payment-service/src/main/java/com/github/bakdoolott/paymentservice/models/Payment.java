aae1d3b502f57147f8e5675091f594c8403391a6
Bakdoolott <mirlanuulubakdoolot@gmail.com>
2026-08-09 22:43:55 +0600

    fixing kafka v1

--- payment-service/src/main/java/com/github/bakdoolott/paymentservice/models/Payment.java
+++ payment-service/src/main/java/com/github/bakdoolott/paymentservice/models/Payment.java
@@ -1,7 +1,18 @@
 package com.github.bakdoolott.paymentservice.models;
 
 import com.github.bakdoolott.paymentservice.models.enums.PaymentStatus;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
