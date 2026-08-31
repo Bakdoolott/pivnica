a186ffb8f392f1f2657c4f063513e0e0a4a9ee32
Yershek <homohojel3@gmail.com>
2026-08-31 21:06:45 +0600

    add bot user

--- auth_service/src/main/java/com/example/auth_service/service/impl/AuthServiceImpl.java
+++ auth_service/src/main/java/com/example/auth_service/service/impl/AuthServiceImpl.java
@@ -93,12 +93,18 @@ public class AuthServiceImpl implements AuthService {
     public TokenResponse verify(String phoneNumber, String code) {
         UserEntity user = userService.findByPhoneNumber(phoneNumber);
 
-        if(user.getId() == 2){
-            return issueTokenPair(user);
-        }
 
         TemporaryCodeEntity active = temporaryCodeRepository.findByUser_IdAndEnableTrue(user.getId())
                 .orElseThrow(() -> new RuntimeException("Неверный или истёкший код"));
+
+        if(user.getId() == 2){
+            if (!active.getCode().equals(code)) {
+                active.setAttempts(active.getAttempts() + 1);
+                temporaryCodeRepository.save(active);
+                throw new RuntimeException("Неверный код");
+            }
+            return issueTokenPair(user);
+        }
 
         if (active.getExpiresAt().isBefore(Instant.now())) {
             active.setEnable(false);
--- auth_service/src/main/resources/db/migration/V2.1788185262__create_test_bot.sql
+++ auth_service/src/main/resources/db/migration/V2.1788185262__create_test_bot.sql
@@ -5,3 +5,6 @@ INSERT INTO user_roles (user_id, role)
 VALUES (2, 'OWNER'),
        (2, 'ADMIN'),
        (2, 'USER');
+
+INSERT INTO temporary_code_tb(id_user_db, code, created_at, expires_at, enable)
+VALUES (2, '1234', '2026-08-31 14:50:56.187364' ,'2026-08-31 14:52:56.187364', 'true');
