76ec4b89e13b298a19997c8799118fc5f1adee78
Yershek <homohojel3@gmail.com>
2026-09-01 11:53:45 +0600

    i removed test bot from dev

--- auth_service/src/main/java/com/example/auth_service/service/impl/AuthServiceImpl.java
+++ auth_service/src/main/java/com/example/auth_service/service/impl/AuthServiceImpl.java
@@ -93,10 +93,6 @@ public class AuthServiceImpl implements AuthService {
     public TokenResponse verify(String phoneNumber, String code) {
         UserEntity user = userService.findByPhoneNumber(phoneNumber);
 
-        if(user.getId() == 2){
-            return issueTokenPair(user);
-        }
-
         TemporaryCodeEntity active = temporaryCodeRepository.findByUser_IdAndEnableTrue(user.getId())
                 .orElseThrow(() -> new RuntimeException("Неверный или истёкший код"));
 
